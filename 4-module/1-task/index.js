64f6bdde256178b70ee85d2fddab44834ae4f0d4
Nastya Shmatova <ponchanastya@gmail.com>
2022-02-14 18:25:41 +0200

    add tasks module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const nameArray = friends.map((el) => `${el.firstName} ${el.lastName}`);
+  const ul = document.createElement('ul');
+  for (let name of nameArray) {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.append(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,39 @@
 function highlight(table) {
-  // ваш код...
+  const tableRows = table.tBodies.item(0).children;
+  for (let i = 0; i < tableRows.length; i++) {
+    const availability = tableRows[i].lastElementChild.getAttribute('data-available');
+    const age = tableRows[i].children[1].innerText;
+    const gender = tableRows[i].children[2].innerText;
+    setAvailabilityIdentification(tableRows[i], availability);
+    setGenderIdentification(tableRows[i], gender);
+    highlightMinorAge(tableRows[i], age);
+  }
+}
+
+function setAvailabilityIdentification(element, availability) {
+  switch (availability) {
+    case "true":
+    element.classList.add('available');
+      break;
+    case "false":
+      element.classList.add('unavailable');
+      break;
+    case null:
+      element.setAttribute('hidden','hidden');
+      break;
+  }
+}
+
+function setGenderIdentification(element, gender) {
+  if (gender === 'm') {
+    element.classList.add('male');
+  } else {
+    element.classList.add('female');
+  }
+}
+
+function highlightMinorAge(element, age) {
+  if (age < 18 ) {
+    element.style = "text-decoration: line-through";
+  }
 }
